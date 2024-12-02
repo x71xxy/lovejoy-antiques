cda4b16d5d4adc31536a09f82787a276e96f98b1
x71 <1249192949@qq.com>
2024-12-02 23:17:53 +0000

    Fix1

--- app/config.py
+++ app/config.py
@@ -12,6 +12,17 @@ class Config:
     RECAPTCHA_OPTIONS = {'theme': 'light'}
     
     # 邮件配置
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.qq.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = ('Lovejoy Antiques', os.environ.get('MAIL_USERNAME'))
+    
+    # 邮件验证配置
+    VERIFY_EMAIL_SALT = 'email-verification'
+    
+    # 邮件配置
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
